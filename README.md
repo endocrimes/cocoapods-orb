a184147a6cc2d31d8c341450e6760ef143b15687
Danielle Tomlinson <dani@tomlinson.io>
2019-02-03 12:13:28 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cocoapods-orb
+
+An orb for performing common CocoaPods related tasks on [CircleCI](http://circleci.com).
+
---
+++ test/install/Orb ExampleTests/Orb_ExampleTests.swift
@@ -0,0 +1,21 @@
+//
+//  Orb_ExampleTests.swift
+//  Orb ExampleTests
+//
+//  Created by Dani Tomlinson on 2/3/19.
+//  Copyright © 2019 Dani Tomlinson. All rights reserved.
+//
+
+import XCTest
+@testable import Orb_Example
+
+class Orb_ExampleTests: XCTestCase {
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func testExample() {
+    }
+}
